0158d804abe8da31a778210adac0f49d9d474609
NETHinji <44230892+NETHinji@users.noreply.github.com>
2018-10-29 16:04:58 +0530

    Add files via upload

---
+++ Customer System/CustomerSystem/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSystem
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public DateTime DateOfbirth { get; set; }
+
+    }
+    class CustomerBO
+    {
+        static List<Customer> customers = null;
+
+        public CustomerBO()
+        {
+            cList = new List<Customer>();
+            public bool AddCustomer(Customer custObj)
+            {
+                cList.Add(custObj);
+            }
+            public void ExportCustomerReport(string reportType)
+            {
+                if (reportType == "doc")
+                {
+
+                }
+            }
+
+        }
+    }
+}
